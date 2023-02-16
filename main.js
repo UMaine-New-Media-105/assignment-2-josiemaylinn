763dce9ffdca1859607baee75edbaa74ef51687b
josiemaylinn <124173145+josiemaylinn@users.noreply.github.com>
2023-02-15 20:17:47 -0500

    Question 2

--- main.js
+++ main.js
@@ -2,77 +2,18 @@ function setup() {
   createCanvas(400, 400);
 }
 
-function setup() {
-  createCanvas(400, 400);
-}
-
 function draw() {
-  rowOne()
-  rowTwo()
-  rowThree()
-  rowFour()
-  rowFive()
-}
+  background(220);
 
-function rowOne() {
-  y1 = 0
-  fill('red')
-  square(0, y1, 80);
-  square(320, y1, 80);
-  fill('white')
-  square(80, y1, 80);
-  square(160, y1, 80);
-  square(240, y1, 80);
-  
-}
-
-function rowTwo() {
-  y2 = 80
-  fill('red')
-  square(0, y2, 80);
-  square(80, y2, 80);
-  square(240, y2, 80);
-  square(320, y2, 80);
-  fill('white')
-  square(160, y2, 80);
-
-  
-}
-
-function rowThree() {
-  y3 = 160
-  fill('gold')
-  square(0, y3, 80);
-  square(320, y3, 80);
-  fill('white')
-  square(160, y3, 80);
-  fill('red')
-  square(80, y3, 80);
-  square(240, y3, 80);
-
-}
-
-function rowFour() {
-  y4 = 240
-  fill('red')
-  square(0, y4, 80);
-  square(80, y4, 80);
-  square(240, y4, 80);
-  square(320, y4, 80);
-  fill('white')
-  square(160, y4, 80);
-
-  
-}
-
-function rowFive() {
-  y5 = 320
-  fill('red')
-   square(0, 320, 80);
-  square(240, 320, 80);
-  square(320, 320, 80);
-  fill('white')
-    square(80, 320, 80);
-  square(160, 320, 80);
-  
+  stroke(1);
+  strokeWeight(5);
+  beginShape();
+  vertex(50, 100);
+  vertex(200, 25);
+  vertex(200, 200);
+  vertex(350, 200);
+  vertex(200, 375);
+  vertex(50, 200);
+  vertex(50, 100);
+  endShape();
 }
